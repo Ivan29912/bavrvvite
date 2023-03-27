9c1eee0e028b74f259ed4719b05f4f9959b7efd1
Ivan29912 <129027689+Ivan29912@users.noreply.github.com>
2023-03-27 17:34:12 +0000

    section 1 complete

--- src/components/Bodyappsection1.jsx
+++ src/components/Bodyappsection1.jsx
@@ -5,55 +5,25 @@ const Bodyappsection1 = () => {
     return (
         <section className='section-inverter'>
             <picture>
-                {sectionJSON.map(sec => sec.section === "1" ? <img className='img' src={sec.img} alt='papa'/> : "") }
+                {sectionJSON.map(sec => sec.section === "1" ? <img className='img' src={sec.img} alt='papa' /> : "")}
             </picture>
             <div className='div-inverter'>
-                <h5> {sectionJSON} </h5>
-                <h2>Una nueva generación <br /> de equipos de aire</h2>
-                <p>Somos expetos en aire acondicionado.<br /> Por eso, desarrollamos una opción ideal<br /> para cada tipo de aplicación.</p>
+                <h5>{sectionJSON.map(sec => sec.section === "1" ? sec.concept : "")}</h5>
+                <h2>{sectionJSON.map(sec => sec.section === "1" ? sec.title : "")}</h2>
+                <p>{sectionJSON.map(sec => sec.section === "1" ? sec.slogan : "")}</p>
                 <div className='ul-list'>
                     <div className='ul-list-a'>
                         <ul>
-                            <li>
-                                Mini y Multi Split.
-                            </li>
-                            <li>
-                                Cassette.
-                            </li>
-                            <li>
-                                Baja silueta.
-                            </li>
-                            <li>
-                                Rooftop.
-                            </li>
-                            <li>
-                                Enfriadoras (chillers).
-                            </li>
-                            <li>
-                                Manejadoras de aire.
-                            </li>
+                            {
+                                sectionJSON.map(sec => sec.section === "1" ? sec.details[0].map(detail => <li>{detail}</li>) : "")
+                            }
                         </ul>
                     </div>
                     <div className='ul-list-b'>
                         <ul>
-                            <li>
-                                Sistemas VRV.
-                            </li>
-                            <li>
-                                Piso / Techo.
-                            </li>
-                            <li>
-                                Calefactor.
-                            </li>
-                            <li>
-                                Sistemas de control.
-                            </li>
-                            <li>
-                                Sistemas aplicados.
-                            </li>
-                            <li>
-                                Fan coils.
-                            </li>
+                            {
+                                sectionJSON.map(sec => sec.section === "1" ? sec.details[1].map(detail => <li>{detail}</li>) : "")
+                            }
                         </ul>
                     </div>
                 </div>
